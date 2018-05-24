a6cc8967b9194c1b684c2bfa2750168528edcd71
Bovey0809 <my364007886@gmail.com>
2018-05-24 09:29:04 +0800

    test shadow socks

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        String numbers = "//[***][%]\n1***2%3";
+
+        StringCalculator calculator = new StringCalculator();
+        int output = calculator.add(numbers);
+        System.out.println(" output = " + output);
+
+    }
 }
